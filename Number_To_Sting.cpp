b0ad9fcfc66356da318446c37ebb3e543432c175
VivekBhat17 <Vivekbhat679@gmail.com>
2021-07-07 12:58:29 +0530

    First commit

---
+++ Number_To_Sting.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+string NumberToString(int num){   //Number to string conversion.
+string s = " ";
+while(num){
+int rem = num%10;
+s += (rem+'0');
+num /= 10;
+}
+return s;
+}
+
+int main(){						// Main function.
+int num =100,i;
+string s = NumberToString(num) ; //Function call.
+i = s.size();
+cout<<"' ";
+while(i-- != 0){
+cout<<s[i];
+}
+cout<<"'";
+}
